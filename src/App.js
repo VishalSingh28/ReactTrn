24dfd2178e6286de36ea7567c771aef7a9947a01
VishalSingh28 <visusingh009@gmail.com>
2021-06-03 20:05:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vishal <code>src/App.js</code> training.
         </p>
         <a
           className="App-link"
---
+++ src/components/workTrn.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Work extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Work;
