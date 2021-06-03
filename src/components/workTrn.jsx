1014695a0e77ca306db041dd12324f02dada5ba6
VishalSingh28 <visusingh009@gmail.com>
2021-06-03 20:08:17 +0530

    initia commit

--- src/components/workTrn.jsx
+++ src/components/workTrn.jsx
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 class Work extends Component {
     state = {  }
     render() { 
-        return (  );
+        return ( 
+            <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Vishal <code>src/App.js</code> training.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+         );
     }
 }
  
