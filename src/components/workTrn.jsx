1628fc396808b091aa216e36f446fd7a6489b55d
VishalSingh28 <visusingh009@gmail.com>
2021-06-04 14:38:25 +0530

    component commit

--- src/components/workTrn.jsx
+++ src/components/workTrn.jsx
@@ -2,24 +2,12 @@ import React, { Component } from 'react';
 class Work extends Component {
     state = {  }
     render() { 
-        return ( 
-            <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vishal <code>src/App.js</code> training.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-         );
+        return (
+        <div>
+          <h1>Hello World Vishal Singh</h1>
+          <button>Increment</button>
+        </div>
+          );
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Work from './components/workTrn'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Work />
   </React.StrictMode>,
   document.getElementById('root')
 );
